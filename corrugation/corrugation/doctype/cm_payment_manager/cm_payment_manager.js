788c86d3d8b874576a31c0adf1c0d19e44e327ad
Sathisha Poojary <sathishpy@gmail.com>
2017-11-10 01:18:24 +0530

    https://github.com/sathishpy/corrugation/issues/13 Incorporate both reconcilliation and payment creation in a single page. Make some of the entries easier by using difflib. Also use this mainly as bank statement manager.

--- corrugation/corrugation/doctype/cm_payment_manager/cm_payment_manager.js
+++ corrugation/corrugation/doctype/cm_payment_manager/cm_payment_manager.js
@@ -2,6 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('CM Payment Manager', {
+	setup: function(frm) {
+		frm.events.account_filters(frm)
+	},
 	refresh: function(frm) {
 
 	},
@@ -16,8 +19,31 @@ frappe.ui.form.on('CM Payment Manager', {
 			}
 		});
 	},
+	account_filters: function(frm) {
+		frm.fields_dict['bank_account'].get_query = function(doc, dt, dn) {
+			return {
+				filters:[
+					["Account", "account_type", "in", ["Bank"]]
+				]
+			}
+		};
+		frm.fields_dict['receivable_account'].get_query = function(doc, dt, dn) {
+			return {
+				filters:[
+					["Account", "account_type", "in", ["Receivable"]]
+				]
+			}
+		};
+		frm.fields_dict['payable_account'].get_query = function(doc, dt, dn) {
+			return {
+				filters:[
+					["Account", "account_type", "in", ["Payable"]]
+				]
+			}
+		};
+	},
 
-	get_payment_entries: function(frm) {
+	get_payments: function(frm) {
 		frm.events.invoke_doc_function(frm, "populate_payment_entries");
 	},
 
@@ -26,5 +52,8 @@ frappe.ui.form.on('CM Payment Manager', {
 	},
 	create_payments: function(frm) {
 		frm.events.invoke_doc_function(frm, "create_payment_entries");
+	},
+	submit_payments: function(frm) {
+		frm.events.invoke_doc_function(frm, "submit_payment_entries");
 	},
 });
--- corrugation/corrugation/doctype/cm_payment_manager/cm_payment_manager.py
+++ corrugation/corrugation/doctype/cm_payment_manager/cm_payment_manager.py
@@ -9,15 +9,12 @@ from erpnext.accounts.utils import get_outstanding_invoices
 from frappe.utils import nowdate
 from datetime import datetime
 import csv, os, re
+import difflib
 
 class CMPaymentManager(Document):
 	def autoname(self):
-		if (self.bank_statement):
-			if (not self.from_date or not self.to_date):
-				frappe.throw("Enter from and to date for bank statement entries")
-			self.name = self.bank_account + "-" + self.from_date + "-" + self.to_date
-		else:
-			self.name = self.bank_account + nowdate()
+		self.name = self.bank_account + "-" + self.from_date + "-" + self.to_date
+		self.new_transaction_items = []
 
 	def populate_payment_entries(self):
 		if self.bank_statement is None: return
@@ -25,33 +22,39 @@ class CMPaymentManager(Document):
 		filepath = frappe.get_site_path("private", "files", filename);
 		if (not os.path.isfile(filepath)):
 			filepath = frappe.get_site_path("public", "files", filename);
+		if (len(self.new_transaction_items + self.reconciled_transaction_items) > 0):
+			frappe.throw("Transactions already retreived from the statement")
 
 		with open(filepath) as csvfile:
 			entries = csv.DictReader(csvfile)
-			self.payment_entry_items = []
 			for entry in entries:
-				print("Processing entry DESC:{0}-W:{1}-D:{2}".format(entry["Particulars"], entry["Withdrawals"], entry["Deposits"]))
-				date = entry["Date"]
+				date = entry["Date"].strip()
 				if (not date): continue
-				bank_entry = frappe.new_doc("CM Payment Entry Item")
-				bank_entry.transaction_date = datetime.strptime(date, '%d-%m-%Y').date()
-				if (self.from_date and bank_entry.transaction_date < datetime.strptime(self.from_date, '%Y-%m-%d').date()): continue
-				if (self.to_date and bank_entry.transaction_date > datetime.strptime(self.to_date, '%Y-%m-%d').date()): continue
+				transaction_date = datetime.strptime(date, '%d-%m-%Y').date()
+				if (self.from_date and transaction_date < datetime.strptime(self.from_date, '%Y-%m-%d').date()): continue
+				if (self.to_date and transaction_date > datetime.strptime(self.to_date, '%Y-%m-%d').date()): continue
+				print("Processing entry DESC:{0}-W:{1}-D:{2}-DT:{3}".format(entry["Particulars"], entry["Withdrawals"], entry["Deposits"], entry["Date"]))
+				bank_entry = self.append('new_transaction_items', {})
+				bank_entry.transaction_date = transaction_date
 				bank_entry.description = entry["Particulars"]
 				bank_entry.party_type = "Supplier" if not entry["Deposits"].strip() else "Customer"
+				party_list = frappe.get_all(bank_entry.party_type, fields=["name"])
+				parties = [party.name for party in party_list]
+				matches = difflib.get_close_matches(bank_entry.description.lower(), parties, 1, 0.4)
+				if len(matches) > 0: bank_entry.party = matches[0]
+				print("Finding {0} in {1}".format(bank_entry.description.lower(), bank_entry.party))
 				amount = entry["Withdrawals"] if not entry["Deposits"].strip() else entry["Deposits"]
-				print("Type: {0} amount:{1}".format(bank_entry.party_type, amount))
 				bank_entry.amount = float(amount)
-				self.append("payment_entry_items", bank_entry)
 
 	def populate_matching_invoices(self):
 		self.payment_invoice_items = []
-		for entry in self.payment_entry_items:
+		for entry in self.new_transaction_items:
 			if (not entry.party): continue
 			account = self.receivable_account if entry.party_type == "Customer" else self.payable_account
 			outstanding_invoices = get_outstanding_invoices(entry.party_type, entry.party, account)
 			sorted(outstanding_invoices, key=lambda k: k['posting_date'])
 			amount = entry.amount
+			matching_invoices = []
 			for e in outstanding_invoices:
 				ent = self.append('payment_invoice_items', {})
 				ent.invoice_date = e.get('posting_date')
@@ -61,13 +64,48 @@ class CMPaymentManager(Document):
 				ent.outstanding_amount = e.get('outstanding_amount')
 				ent.allocated_amount = min(float(e.get('invoice_amount')), amount)
 				amount -= float(e.get('invoice_amount'))
+				matching_invoices += [ent.invoice]
 				if (amount <= 0): break
 
+			order_doctype = "Sales Order" if entry.party_type=="Customer" else "Purchase Order"
+			from erpnext.controllers.accounts_controller import get_advance_payment_entries
+			payment_entries = get_advance_payment_entries(entry.party_type, entry.party, account, order_doctype, against_all_orders=True)
+			payment = next((payment for payment in payment_entries if payment.amount == entry.amount), None)
+			if (payment is None): continue
+			doc = frappe.get_doc(payment.reference_type, payment.reference_name)
+			added = next((entry.payment_entry for entry in self.payment_items if entry.payment_entry == doc.name), None)
+			if added is not None: continue
+			invoices = [entry.invoice for entry in doc.references]
+			self.append('payment_items', {"payment_type": doc.doctype,
+											"payment_entry": doc.name,
+											'party_type': entry.party_type,
+											"mode_of_payment": doc.mode_of_payment,
+											"reference": doc.reference_no,
+											"invoice_type": "Sales Invoice" if entry.party_type == "Customer" else "Purchase Invoice",
+											"account": account,
+											"invoices": ",".join(invoices + matching_invoices),
+											"paid_amount": doc.paid_amount
+										})
+			entry.reference_name = payment.reference_name
+			entry.reference_type = payment.reference_type
+
 	def create_payment_entries(self):
-		for payment_entry in self.payment_entry_items:
+		for payment_entry in self.new_transaction_items:
 			if (not payment_entry.party): continue
 			print("Creating payment entry for {0}".format(payment_entry.description))
-			self.make_customer_payment(payment_entry)
+			payment = self.make_customer_payment(payment_entry)
+			invoices = [entry.invoice for entry in payment.references if entry is not None]
+			self.append('payment_items', {"payment_type": payment.doctype,
+											"payment_entry": payment.name,
+											"mode_of_payment": payment.mode_of_payment,
+											'party_type': payment_entry.party_type,
+											"reference": payment.reference_no,
+											"invoices": ",".join(invoices),
+											"paid_amount": payment.paid_amount
+										})
+			payment_entry.reference_name = payment.name
+			payment_entry.reference_type = payment.doctype
+		msgprint(_("Successfully created payment entries"))
 
 	def make_customer_payment(self, pe):
 		payment = frappe.new_doc("Payment Entry")
@@ -95,7 +133,39 @@ class CMPaymentManager(Document):
 		#payment.set_amounts()
 		#print("Created payment entry {0}".format(payment.as_dict()))
 		payment.save()
+		return payment
 
-	def on_update(self):
-		self.populate_payment_entries()
-		#self.populate_matching_invoices()
+	def update_payment_entry(self, payment):
+		lst = []
+		invoices = payment.invoices.split(',')
+		for invoice in invoices:
+			lst.append(frappe._dict({
+				'voucher_type': payment.payment_type,
+				'voucher_no' : payment.payment_entry,
+				'against_voucher_type' : payment.invoice_type,
+				'against_voucher'  : invoice,
+				'account' : payment.account,
+				'party_type': payment.party_type,
+				'party': frappe.get_value("Payment Entry", payment.payment_entry, "party"),
+				'unadjusted_amount' : 0,
+				'allocated_amount' : float(payment.paid_amount)
+			}))
+		if lst:
+			from erpnext.accounts.utils import reconcile_against_document
+			reconcile_against_document(lst)
+
+	def submit_payment_entries(self):
+		for payment in self.payment_items:
+			doc = frappe.get_doc(payment.payment_type, payment.payment_entry)
+			if doc.docstatus == 1:
+				self.update_payment_entry(payment)
+			else:
+				doc.reference_no = payment.reference
+				doc.mode_of_payment = payment.mode_of_payment
+				doc.save()
+				doc.submit()
+
+		for entry in self.new_transaction_items:
+			if entry.reference_type is not None:
+				self.remove(entry)
+				self.append('reconciled_transaction_items', entry)
